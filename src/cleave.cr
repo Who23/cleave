4517875efd8380d72646ca7af46173a29931ef25
Who23 <contactwho23@gmail.com>
2020-11-30 16:03:43 -0500

    move around CLI

--- src/cleave.cr
+++ src/cleave.cr
@@ -1,41 +1,21 @@
 require "clim"
+require "./shamir.cr"
+require "./cli.cr"
+
 
 module Cleave
   VERSION = "0.1.0"
 
-  Cli.start(ARGV)
-end
-
-class Cli < Clim
-  main do
-    desc "Cleave - Encrypt a file and split its key into multiple parts"
-    usage "cleave [subcommand] [options] [arguments]"
-    run do |opts, args|
-      puts opts.help_string
-    end
-
-    sub "fuse" do 
-      desc "Fuse the original pieces of the file to obtain the original file"
-      usage "cleave fuse <file1>...<fileN>"
-      run do |opts, args|
-        if args.unknown_args.size > 0
-          puts "run fuse"
-        else
-          puts opts.help_string
-        end
-      end
-    end
-
-    sub "split" do 
-      desc "Split a file into N encrypted pieces, requiring T pieces to reconstruct the file"
-      usage "cleave split [options] <file>"
-      option "-n PIECES", type: Int32, desc: "Number of pieces generated", default: 5
-      option "-t REQ_PIECES", type: Int32, desc: "Number of pieces required to reconstruct the file", default: 3
-      argument "file", type: String, desc: "File to split", required: true
-      run do |opts, args|
-        puts "run split"
-      end
-    end
-
+  def self.split(pieces : Int32, req_pieces : Int32, filename : String)
+    # AES.encrypt(filename, "out.cleave")
+    # encrypt file
+    # encrypt key 
+    # construct split files.
   end
+
+  def self.fuse(files : Array(String))
+    # AES.decrypt(files[0], files[1], File.read("password"))
+  end
+
+  Cli.start(ARGV)
 end
---
+++ src/cli.cr
@@ -0,0 +1,35 @@
+# require "./cleave.cr"
+
+class Cli < Clim
+  main do
+    desc "Cleave - Encrypt a file and split its key into multiple parts"
+    usage "cleave [subcommand] [options] [arguments]"
+    run do |opts, args|
+      puts opts.help_string
+    end
+
+    sub "fuse" do 
+      desc "Fuse the original pieces of the file to obtain the original file"
+      usage "cleave fuse <file1>...<fileN>"
+      run do |opts, args|
+        if args.unknown_args.size > 0
+          Cleave.fuse(args.unknown_args)
+        else
+          puts opts.help_string
+        end
+      end
+    end
+
+    sub "split" do 
+      desc "Split a file into N encrypted pieces, requiring T pieces to reconstruct the file"
+      usage "cleave split [options] <file>"
+      option "-n PIECES", type: Int32, desc: "Number of pieces generated", default: 5
+      option "-t REQ_PIECES", type: Int32, desc: "Number of pieces required to reconstruct the file", default: 3
+      argument "file", type: String, desc: "File to split", required: true
+      run do |opts, args|
+        Cleave.split(opts.n, opts.t, args.file)
+      end
+    end
+
+  end
+end
